2cf8109540186083cf023ccaa338fb7f7e57e991
glorygollamudi <113373896+glorygollamudi@users.noreply.github.com>
2023-06-26 22:38:28 +0530

    Add files via upload

---
+++ rbl.py
@@ -0,0 +1,76 @@
+# importing the libraries
+from tensorflow.keras.layers import Input, Lambda, Dense, Flatten,Conv2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator,load_img
+from tensorflow.keras.models import Sequential
+import numpy as np
+from glob import glob
+Import matplotlib.pyplot as plt
+# re-size all the images to this
+IMAGE_SIZE = [224, 224]
+train_path = '/content/drive/MyDrive/skin_dataset/dataset/train'
+valid_path = '/content/drive/MyDrive/skin_dataset/dataset/train'
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+config = ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+resnet = ResNet50(input_shape=IMAGE_SIZE + [3], weights='imagenet', include_top=False)
+# don't train existing weights
+for layer in resnet.layers:
+ layer.trainable = False
+ # useful for getting number of output classes
+folders = glob('/content/drive/MyDrive/skin_dataset/dataset/train/*')
+folders
+# our layers - you can add more if you want
+x = Flatten()(resnet.output)
+prediction = Dense(len(folders), activation='softmax')(x)
+# create a model object
+model = Model(inputs=resnet.input, outputs=prediction)
+# view the structure of the model
+model.summary()
+From tensorflow.keras.layers import MaxPooling2D
+### Create Model from scratch using CNN
+model=Sequential()
+model.add(Conv2D(filters=16,kernel_size=2,padding="same",activation="relu",input_shape=(224,224,3)))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=32,kernel_size=2,padding="same",activation ="relu"))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(filters=64,kernel_size=2,padding="same",activation="relu"))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(500,activation="relu"))
+model.add(Dense(2,activation="softmax"))
+model.summary()
+# tell the model what cost and optimization method to use
+model.compile(
+ loss='categorical_crossentropy',
+ optimizer='adam',
+ metrics=['accuracy']
+)
+# Use the Image Data Generator to import the images from the dataset
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+train_datagen = ImageDataGenerator(rescale = 1./255,shear_range = 0.2,zoom_range = 0.2,horizontal_flip = True)
+test_datagen = ImageDataGenerator(rescale = 1./255)
+# Make sure you provide the same target size as initialied for the imag
+e size
+training_set = train_datagen.flow_from_directory('/content/drive/MyDrive/skin_dataset/dataset/train',target_size = (224, 224),batch_size = 32,class_mode = 'categorical')
+test_set = test_datagen.flow_from_directory('/content/drive/MyDrive/skin_dataset/dataset/test',target_size = (224, 224),batch_size = 32,class_mode = 'categorical')
+# fit the model
+# Run the cell. It will take some time to execute
+r = model.fit_generator(training_set,validation_data=test_set,epochs=10,steps_per_epoch=len(training_set),validation_steps=len(test_set))
+# plot the loss
+plt.plot(r.history['loss'], label='train loss')
+plt.plot(r.history['val_loss'], label='val loss')
+plt.legend()
+plt.show()
+plt.savefig('LossVal_loss')
+# plot the accuracy
+plt.plot(r.history['accuracy'], label='train acc')
+plt.plot(r.history['val_accuracy'], label='val acc')
+plt.legend()
+plt.show()
+plt.savefig('AccVal_acc')
